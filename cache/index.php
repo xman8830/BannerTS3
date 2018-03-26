af4da1b4c34645887fc9ad92b046be839a6216d8
xman8830 <xman8830@gmail.com>
2018-03-26 14:25:16 +0200

    Add files via upload

---
+++ cache/index.php
@@ -0,0 +1,4 @@
+Not Found
+The requested URL <?php echo $_SERVER["REQUEST_URI"];?> was not found on this server.
+
+<?php echo $_SERVER["SERVER_SOFTWARE"];?> Server at <?php echo $_SERVER["HTTP_HOST"]; ?> Port 80
